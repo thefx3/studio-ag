723ddd99fa3da74945d56c222903f237e50055a7
thefx3 <fx.pairault@yahoo.fr>
2025-10-12 00:49:11 +0200

    Responsivness header, burger style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,44 @@
-import { Link } from "react-router-dom";
+// Header.jsx
+import { useState } from "react";
 import Button from "./Button";
 import "./Header.css";
 import logo from "../images/logo.png";
 import send from "../images/send.png";
+import menu from "../images/menu.png";
+import close from "../images/close.png";
 
 
 function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => setMenuOpen(!menuOpen);
+
   return (
     <header className="header">
-      <nav className="header-left">
+      <div className="header-left">
         <img src={logo} alt="Studio AG Logo" className="logo-image" />
         <Button text="STUDIO AG" to="home" />
-      </nav>
-      <nav className="header-middle">
+      </div>
+
+      {/* Bouton burger visible sur mobile */}
+      <div className="burger" onClick={toggleMenu}>
+        <img
+          src={menuOpen ? close : menu}
+          alt="Menu"
+          className="burger-icon"
+        />
+      </div>
+
+      <nav className={`header-middle ${menuOpen ? "active" : ""}`}>
         <Button text="Services et Tarifs" to="services" />
         <Button text="La Brique Studio" to="studio" />
         <Button text="Qui suis-je ?" to="about" />
       </nav>
-      <nav className="header-right">
+
+      <div className="header-right">
         <img src={send} alt="Send Logo" className="icon" />
         <Button text="Contact" to="contact" />
-      </nav>
+      </div>
     </header>
   );
 }
